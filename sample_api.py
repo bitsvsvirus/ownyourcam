8777926691423ef3bbac293a6781b8a0536c624a
Vinh Ngu <vinh-ngu@hotmail.com>
2020-04-25 22:01:46 +0200

    Refactor code.

--- sample_api.py
+++ sample_api.py
@@ -5,15 +5,11 @@ import requests
 rcam = cv2.VideoCapture(0)
 width, height = int(rcam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(rcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # width, height = 640, 480
+
+# Configure Videocapture
 rcam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 rcam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 fgbg = cv2.createBackgroundSubtractorKNN()
-
-
-def denoise(frame):
-    frame = cv2.medianBlur(frame, 5)
-    frame = cv2.GaussianBlur(frame, (5, 5), 0)
-    return frame
 
 
 def get_mask(frame, bodypix_url='http://localhost:9000'):
@@ -38,7 +34,7 @@ while True:
     # frame = denoise(frame)
     cv2.imshow('Original', frame)
     mask = get_mask(frame)
-    cv2.normalize(mask, 0, 255, cv2.NORM_MINMAX)
+    # cv2.normalize(mask, 0, 255, cv2.NORM_MINMAX)
     cv2.imshow('Mask', mask * 255)
 
     # Merge original frame and its mask.
