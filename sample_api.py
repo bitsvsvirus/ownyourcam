84c2ff739e2bb63dce90725c88bf1546a5ee8a6b
Vinh Ngu <vinh-ngu@hotmail.com>
2020-04-25 22:04:32 +0200

    Delete unused methods.

--- sample_api.py
+++ sample_api.py
@@ -2,13 +2,13 @@ import cv2
 import numpy as np
 import requests
 
-rcam = cv2.VideoCapture(0)
-width, height = int(rcam.get(cv2.CAP_PROP_FRAME_WIDTH)), int(rcam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+videocap = cv2.VideoCapture(0)
+width, height = int(videocap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(videocap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 # width, height = 640, 480
 
 # Configure Videocapture
-rcam.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-rcam.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+videocap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+videocap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 fgbg = cv2.createBackgroundSubtractorKNN()
 
 
@@ -26,26 +26,24 @@ def get_mask(frame, bodypix_url='http://localhost:9000'):
 
 
 # Create a red background.
-replacement_bg = np.zeros((height, width, 3), dtype=np.uint8)
-replacement_bg[:, :, 2] = 255
+background = np.zeros((height, width, 3), dtype=np.uint8)
+background[:, :, 2] = 255
 
 while True:
-    ret, frame = rcam.read()
-    # frame = denoise(frame)
+    ret, frame = videocap.read()
     cv2.imshow('Original', frame)
     mask = get_mask(frame)
-    # cv2.normalize(mask, 0, 255, cv2.NORM_MINMAX)
     cv2.imshow('Mask', mask * 255)
 
     # Merge original frame and its mask.
     inv_mask = 1 - mask
     for c in range(frame.shape[2]):
-        frame[:, :, c] = frame[:, :, c] * mask + replacement_bg[:, :, c] * inv_mask
+        frame[:, :, c] = frame[:, :, c] * mask + background[:, :, c] * inv_mask
 
     cv2.imshow('Merged', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything done, release the capture
-rcam.release()
+videocap.release()
 cv2.destroyAllWindows()
