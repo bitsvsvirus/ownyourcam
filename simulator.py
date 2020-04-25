1a5a9d2e03793e85205cfd8a2195eb0a7be2f239
Vinh Ngu <vinh-ngu@hotmail.com>
2020-04-25 22:49:03 +0200

    Stream faked images to virtualcam device.

--- simulator.py
+++ simulator.py
@@ -6,22 +6,24 @@ from logger import Logger
 
 
 class Simulator:
-    def __init__(self, width=None, height=None, video_source=0, bg_path=None):
+    def __init__(self, width=None, height=None, video_source=0, bg_path=None, vcam_source='/dev/video1'):
         self.logger = Logger.logger
         self.videocap = cv2.VideoCapture(video_source)
         if not width or not height:
             width, height = int(self.videocap.get(cv2.CAP_PROP_FRAME_WIDTH)), \
                             int(self.videocap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # width, height = 640, 480
         self.logger.info('Create a virtualwebcam with a solution of {}x{}'.format(width, height))
 
         # Configure Videocapture
         self.videocap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         self.videocap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 
-        # Create a red background.
+        # Configure background from passed bg_path
         background = cv2.imread(bg_path)
         self.background = cv2.resize(background, (width, height))
+
+        from pyfakewebcam import pyfakewebcam
+        self.vcam = pyfakewebcam.FakeWebcam(vcam_source, width, height)
 
     def get_mask(self, frame, bodypix_url='http://localhost:9000'):
         _, data = cv2.imencode(".jpg", frame)
@@ -33,20 +35,26 @@ class Simulator:
         mask = mask.reshape((frame.shape[0], frame.shape[1]))
         return mask
 
-    def simulate(self):
+    def simulate(self, show_stream=False):
         self.logger.info("Create a virtual webcam.")
         while True:
             ret, frame = self.videocap.read()
-            cv2.imshow('Original', frame)
             mask = self.get_mask(frame)
-            cv2.imshow('Mask', mask * 255)
 
             # Merge original frame and its mask.
             inv_mask = 1 - mask
+            merged = np.copy(frame)
             for c in range(frame.shape[2]):
-                frame[:, :, c] = frame[:, :, c] * mask + self.background[:, :, c] * inv_mask
+                merged[:, :, c] = merged[:, :, c] * mask + self.background[:, :, c] * inv_mask
 
-            cv2.imshow('Merged', frame)
+            if show_stream:
+                cv2.imshow('Original', frame)
+                cv2.imshow('Mask', mask * 255)
+                cv2.imshow('Merged', merged)
+
+            # Write image to virtual cam input.
+            # self.vcam.schedule_frame(merged)
+
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
@@ -57,4 +65,4 @@ class Simulator:
 
 if __name__ == '__main__':
     simulator = Simulator(width=640, height=480, bg_path='assets/background.png')
-    simulator.simulate()
+    simulator.simulate(show_stream=False)
